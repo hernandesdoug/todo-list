bd823ef0959ce49d47f8b0cada2119cd59af2d07
Douglas Hernandes <douglashernandes@icloud.com>
2025-06-16 16:25:29 -0300

    adicionando funcao mudar de lista

--- js/app.js
+++ js/app.js
@@ -6,13 +6,24 @@ function adicionaItem() {
     if(item === ''){
         alert("Você precisa digitar algo para adicionar!");
     }else{
-        document.getElementById("list-itens").appendChild(li);
+        document.getElementById("lista-fazer-li").appendChild(li);
     }
     document.getElementById("item").value = "";
 }
+
+function adicionaListaNova() { 
+    const itemMove = document.getElementById("lista-fazer-li");
+    const listaFeito = document.getElementById("lista-feito-li");
+    if(itemMove && listaFeito){
+        listaFeito.appendChild(itemMove);
+    }
+}
+
 
 document.addEventListener("DOMContentLoaded", function(){
     const adicionar = document.getElementById("btn-add");
     adicionar.addEventListener('click', adicionaItem);
 
+    const itemFazer = document.getElementById("lista-fazer-li");
+    itemFazer.addEventListener('click', adicionaListaNova);
 })
