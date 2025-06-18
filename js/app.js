5775bb381f3095537a6c8a2c463ea3eb33fff7c3
Douglas Hernandes <douglashernandes@icloud.com>
2025-06-18 15:23:53 -0300

    ajuste controle de listas

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 let contador = 0;
 let contadorNovo = 0;
+let retornaLista = false;
 function adicionaItem() {
     if (contador > 11) {
         alert("Chegou ao limite da lista");
@@ -16,34 +17,52 @@ function adicionaItem() {
         alert("Você precisa digitar algo para adicionar!");
     }else{
         document.getElementById("lista-fazer-li").appendChild(li);
+        retornaLista = true;
+        contador++;
     }
     document.getElementById("item").value = "";
-    contador++;
+    console.log("Incrementa lista fazer: " + contador);
 }
 
 function adicionaListaNova(evento) {
     const el = evento.target;
     const listaFeito = document.getElementById("lista-feito-li");
 
-    listaFeito.addEventListener("click", retornaListaAntiga);
-
-    if(el && listaFeito && contadorNovo < 12){
-        contadorNovo++;
-        listaFeito.appendChild(el);
-        contador--;
+    if (contadorNovo > 11) {
+        alert("Chegou ao limite da lista nova!");
+        return;
     }
+
+    console.log(retornaLista)
+    if(!retornaLista){
+        console.log("entrou para retornar")
+        listaFeito.addEventListener("click", retornaListaAntiga);
+    }
+
+    if(el && listaFeito && retornaLista){
+        listaFeito.appendChild(el);
+        contadorNovo++;
+        contador--;
+        retornaLista = false;
+    }
+    console.log("Incrementa lista feito: " + contadorNovo);
+    console.log("Decrementa lista fazer: " + contador);
 }
 
 function retornaListaAntiga(evento) {
     const el = evento.target;
     const listaAntiga = document.getElementById("lista-fazer-li");
+    
+    console.log("Contador antes de retornar " + contador);
 
     if (el && listaAntiga){
-        console.log(contador);
-        contador++;
         listaAntiga.appendChild(el);
         contadorNovo--;
     }
+    retornaLista = true;
+    contador++; 
+    console.log("Decrementa lista nova ao retornar: " + contadorNovo);
+    console.log("Incrementa lista fazer ao retornar: " + contador )
 }
 
 document.addEventListener("DOMContentLoaded", function(){
